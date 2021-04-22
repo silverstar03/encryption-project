73464d638c08d306fd44ee601700349611f62480
silverstar03 <eunbyoul0421@naver.com>
2021-04-23 02:29:57 +0900

    ui 파일들 화면 화면 전환 성공

--- single.py
+++ single.py
@@ -1,42 +1,50 @@
 import sys
 
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QDesktopWidget, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QDesktopWidget, QLineEdit, QPushButton
+from PyQt5 import uic
 
-from start import start
 
-class single(QWidget):
+# single_UI = uic.loadUiType('single_UI.ui')[0]
+
+
+class single(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.singleUI()
+        self.setupUi(self)
+        # self.singleUI()
+
 
     def singleUI(self):
         font1 = QtGui.QFont('나눔바른고딕', 10)
         font1.setBold(True)
 
-        self.title = QLabel('단일 치환 암호화', self)
-        self.title.move(290, 70)
-        self.title.setFont(QtGui.QFont('나눔바른고딕', 25))
+        title = QLabel('단일 치환 암호화', self)
+        title.move(290, 70)
+        title.setFont(QtGui.QFont('나눔바른고딕', 25))
 
-        self.keylbl = QLabel('암호키', self)
-        self.keylbl.move(220, 170)
+        keylbl = QLabel('암호키', self)
+        keylbl.move(220, 170)
 
-        self.key_edit = QLineEdit('', self)
-        self.key_edit.resize(180, 25)
-        self.key_edit.move(320, 160)
+        key_edit = QLineEdit('', self)
+        key_edit.resize(180, 25)
+        key_edit.move(320, 160)
 
-        self.plainlbl = QLabel('평문', self)
+        self.plainbl = QLabel('평문', self)
         self.plainlbl.move(220, 220)
 
         self.plain_edit = QLineEdit('', self)
         self.plain_edit.resize(180, 25)
         self.plain_edit.move(320, 210)
+        # 엔터 치면 발생하게 하는 메서드
+        # self.plain_edit.returnPressed.connect(self.lineEditChanged)
 
         self.encrypt = QPushButton('암호문', self) #encrypt는 암호화라는 뜻
         self.encrypt.toggle()
         self.encrypt.resize(80, 25)
         self.encrypt.move(320, 260)
+        self.encrypt.clicked.connect(self.changeTextFunction)
 
         self.decrypt = QPushButton('복호문', self)
         self.decrypt.toggle()
@@ -57,7 +65,6 @@ class single(QWidget):
         self.decrypt_edit.resize(180, 25)
         self.decrypt_edit.move(320, 360)
 
-
         self.resize(800, 500)
         self.setWindowTitle("단일 치환 암호화")
         self.center()
@@ -69,8 +76,20 @@ class single(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    # def lineEditChanged(self):
+    #     print(self.plain_edit.text())
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = single()
-    sys.exit(app.exec_())
+    def changeTextFunction(self):
+        # self.cipher_edit.setText(self.plain_edit.text())
+        list = []
+        for i in self.key_edit.text():
+            list.append(i)
+
+        for i in range(list):
+            print(i)
+
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = single()
+#     sys.exit(app.exec_())
--- start.py
+++ start.py
@@ -1,48 +1,37 @@
-import subprocess
 import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+main_ui = uic.loadUiType("start_UI.ui")[0]
 
 
-class start(QWidget):
+class StartClass(QMainWindow, main_ui):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-
-    def initUI(self):
-        font1 = QtGui.QFont('나눔바른고딕', 40)
-        font1.setBold(True)
-
-        self.title = QLabel("암호화 프로그램", self)
-        self.title.setFont(font1)
-        self.title.move(230, 100)
-
-        self.singleBtn = QPushButton('단일 치환', self)
-        self.singleBtn.move(220, 260)
-        self.singleBtn.resize(150, 50)
+        self.setupUi(self)
         self.singleBtn.clicked.connect(self.singleBtn_clicked)
-
-        self.multipleBtn = QPushButton('다중 문자 치환', self)
-        self.multipleBtn.move(420, 260)
-        self.multipleBtn.resize(150, 50)
         self.multipleBtn.clicked.connect(self.multiple_clicked)
 
-        self.setWindowTitle("암호화 프로그램")
-        self.resize(800, 500)
-        self.show()
-
     def singleBtn_clicked(self):
-        subprocess.call("single.py", shell=True)
+        self.main_ui = uic.loadUi("single_UI.ui", self)
+        self.backBtn.clicked.connect(self.singleBack_clicked)
+
+    def singleBack_clicked(self):
+        self.main_ui = uic.loadUi("start_UI.ui", self)
+        self.singleBtn_clicked.connect(self.singleBtn_clicked)
 
     def multiple_clicked(self):
-        subprocess.call("multiple.py", shell=True)
+        self.main_ui = uic.loadUi("multiple_UI.ui", self)
+        self.backBtn.clicked.connect(self.multiBack_clicked)
 
+    def multiBack_clicked(self):
+        self.main_ui = uic.loadUi("start_UI.ui", self)
+        self.multipleBtn.clicked.connect(self.multiple_clicked)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = start()
+    main_window = StartClass()
+    main_window.show()
 
-    sys.exit(app.exec_())
+    app.exec_()
